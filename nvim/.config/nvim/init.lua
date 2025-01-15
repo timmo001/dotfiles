273b47cf5d22b2e735bc319b3dcd4ba3bedf0c60
Aidan Timson <aidan@timmo.dev>
2025-01-15 00:53:07 +0000

    Comment

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -49,8 +49,8 @@ require('mason').setup()
 require('mason-lspconfig').setup {
   automatic_installation = true,
   ensure_installed = {
-    "lua-language-server",
     "gopls",
+    -- "lua-language-server",
   },
 }
 
