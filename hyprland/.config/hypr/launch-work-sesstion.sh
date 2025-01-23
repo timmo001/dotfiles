9c973a8174b90f1b4979ede8e85e118486765024
Aidan Timson <aidan@timmo.dev>
2025-01-23 23:16:40 +0000

    Revert "Change"
    This reverts commit 81cb56100a4eaa8fa1366de62bfc4a0ad2ec3adf.

--- hyprland/.config/hypr/launch-work-sesstion.sh
+++ hyprland/.config/hypr/launch-work-sesstion.sh
@@ -1,17 +1,21 @@
 #!/usr/bin/env zsh
 
-goxlr-daemon &
-cd ~/Applications/Postman\ Agent && ./Postman\ Agent &
+$terminal = ghostty
+$browser = flatpak run app.zen_browser.zen
+$discord = discord
+$chrome = google-chrome
+$goxlr = goxlr-daemon
+$slack = slack
+$postmanagent = ~/Applications/Postman Agent/Postman Agent
+
+$goxlr &
+$postmanagent &
 
 # Add a delay for monitors to be set up
 sleep 8
 
-# Left monitor
-slack &
-flatpak run app.zen_browser.zen &
-
-# Right monitor
-ghostty &
-google-chrome --profile-directory="Profile 1" &
-
+$slack &
+$terminal &
+$browser &
+$chrome --profile-directory="Profile 1" &
 
