81cb56100a4eaa8fa1366de62bfc4a0ad2ec3adf
Aidan Timson <aidan@timmo.dev>
2025-01-23 23:11:19 +0000

    Change

--- hyprland/.config/hypr/launch-work-sesstion.sh
+++ hyprland/.config/hypr/launch-work-sesstion.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env zsh
 
-$terminal = ghostty
-$browser = flatpak run app.zen_browser.zen
-$discord = discord
-$chrome = google-chrome
-$goxlr = goxlr-daemon
-$slack = slack
-$postmanagent = ~/Applications/Postman Agent/Postman Agent
-
-$goxlr &
-$postmanagent &
+goxlr-daemon &
+cd ~/Applications/Postman\ Agent && ./Postman\ Agent &
 
 # Add a delay for monitors to be set up
 sleep 8
 
-$slack &
-$terminal &
-$browser &
-$chrome --profile-directory="Profile 1" &
+# Left monitor
+slack &
+flatpak run app.zen_browser.zen &
+
+# Right monitor
+ghostty &
+google-chrome --profile-directory="Profile 1" &
+
 
