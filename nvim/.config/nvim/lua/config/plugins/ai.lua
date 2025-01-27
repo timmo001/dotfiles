9f85b8dae1133d6ac68b40fc0e6b2dc52b803afd
Aidan Timson <aidan@timmo.dev>
2025-01-27 20:02:42 +0000

    Config

--- nvim/.config/nvim/lua/config/plugins/ai.lua
+++ nvim/.config/nvim/lua/config/plugins/ai.lua
@@ -8,6 +8,11 @@ return {
       provider = "copilot",
       behaviour = {
         auto_suggestions = false,
+        auto_set_highlight_group = true,
+        auto_set_keymaps = true,
+        auto_apply_diff_after_generation = false,
+        support_paste_from_clipboard = false,
+        minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
       },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
