2cf965cd9eac0473f2b81d8e0188c52655f2f1d2
Aidan Timson <aidan@timmo.dev>
2025-01-27 18:21:46 +0000

    Update keymaps

--- nvim/.config/nvim/lua/config/keymap.lua
+++ nvim/.config/nvim/lua/config/keymap.lua
@@ -2,18 +2,20 @@
 -- Make sure to set them here to keep init.lua clean
 
 -- Reload
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Reload config" })
+vim.keymap.set("n", "<leader>X", "<cmd>source %<CR>", { desc = "Reload config" })
 
 -- Format
-vim.keymap.set("n", "<space>f", function()
+vim.keymap.set("n", "<leader>f", function()
   require("conform").format({ async = true })
 end, { desc = "Format" })
 
 -- Move current line up or down a line
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "J", "<cmd>m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("n", "<A-j>", "<cmd>m '>+1<CR>gv=gv", { desc = "Move line down" })
+vim.keymap.set("v", "K", "<cmd>m '<-2<CR>gv=gv", { desc = "Move line up" })
+vim.keymap.set("n", "<A-k>", "<cmd>m '<-2<CR>gv=gv", { desc = "Move line up" })
 
--- Prime did this first: https://github.com/ThePrimeagen/init.lua/blob/d92308a63554db8bf8d75de5d41403cc2ddd692a/lua/theprimeagen/remap.lua#L32C1-L32C38
+-- Escape
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Quick delete bindings
