f6bbbc909de6b030ac4cd996faf333c5c5c56fd1
Aidan Timson <aidan@timmo.dev>
2025-01-27 19:31:29 +0000

    Format

--- nvim/.config/nvim/lua/config/plugins/snacks.lua
+++ nvim/.config/nvim/lua/config/plugins/snacks.lua
@@ -10,7 +10,7 @@ return {
         enabled = true,
         sections = {
           { section = "header" },
-          { section = "keys", gap = 1, padding = 1 },
+          { section = "keys",   gap = 1, padding = 1 },
           { section = "startup" },
           {
             section = "terminal",
@@ -200,8 +200,8 @@ return {
           Snacks.toggle.diagnostics():map("<leader>ud")
           Snacks.toggle.line_number():map("<leader>ul")
           Snacks.toggle
-            .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-            :map("<leader>uc")
+              .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+              :map("<leader>uc")
           Snacks.toggle.treesitter():map("<leader>uT")
           Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
           Snacks.toggle.inlay_hints():map("<leader>uh")
