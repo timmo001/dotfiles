62f47776c54d322e5570de76af409e7b83cdc356
Aidan Timson <aidan@timmo.dev>
2025-02-02 17:00:22 +0000

    Add update script

---
+++ update.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+if [[ -z $STOW_FOLDERS ]]; then
+    # Get list of folders in the dotfiles directory
+    STOW_FOLDERS=$(ls -d */ | sed 's/\///g' | tr '\n' ',' | sed 's/,$//')
+fi
+
+if [[ -z $DOTFILES ]]; then
+    DOTFILES=$HOME/.config/dotfiles
+fi
+
+pushd $DOTFILES
+for folder in $(echo $STOW_FOLDERS | sed "s/,/ /g")
+do
+    echo "stow $folder"
+    stow $folder
+done
+popd
+
