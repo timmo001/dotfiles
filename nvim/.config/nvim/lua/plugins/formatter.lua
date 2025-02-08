7858c6bd9eeb66576d6f4286c51b5fe202f3e5b1
Aidan Timson <aidan@timmo.dev>
2025-02-08 01:53:00 +0000

    Formatter

---
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,7 @@
+return {
+  {
+    "steelsojka/headwind.nvim",
+  },
+  { import = "lazyvim.plugins.extras.linting.eslint" },
+  { import = "lazyvim.plugins.extras.formatting.prettier" },
+}
--- nvim/.config/nvim/lua/plugins/headwind.lua
+++
@@ -1,5 +0,0 @@
-return {
-  {
-    "steelsojka/headwind.nvim",
-  },
-}
