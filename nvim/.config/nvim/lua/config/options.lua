c8ea84c519b7a9485b4a91e8b08fe2973b4205ed
Aidan Timson <aidan@timmo.dev>
2025-02-11 11:51:42 +0000

    Wrap enabled

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,5 +2,6 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
--- Set to false to disable auto format
-vim.g.lazyvim_eslint_auto_format = true
+vim.g.lazyvim_eslint_auto_format = true -- Set to false to disable auto format
+
+vim.opt.wrap = true
