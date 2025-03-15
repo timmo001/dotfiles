f553c27b4f048d718b7bc1aab0eeb2b0c7b46129
Aidan Timson <aidan@timmo.dev>
2025-03-15 21:19:06 +0000

    Increase menu size

--- waybar/.config/waybar/powermenu.sh
+++ waybar/.config/waybar/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chosen=$(echo -e " Lock\n⏾ Suspend\n⏻ Power Off\n Reboot\n Logout" | wofi --dmenu --width 300 --height 200 --cache-file /dev/null)
+chosen=$(echo -e " Lock\n⏾ Suspend\n⏻ Power Off\n Reboot\n Logout" | wofi --dmenu --width 320 --height 240 --cache-file /dev/null)
 
 case "$chosen" in
 " Lock") swaylock ;;
