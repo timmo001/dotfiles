3b3fc2b8a262089b2a48958c521896bedb14e1f2
Aidan Timson <aidan@timmo.dev>
2025-03-15 21:44:58 +0000

    Editorconfig

--- windows.ps1
+++ windows.ps1
@@ -1,11 +1,25 @@
 # ------------------------------
+# editorconfig
+# ------------------------------
+
+# Remove old editorconfig file
+if (Test-Path $env:USERPROFILE\.editorconfig) {
+  Write-Host "Remove $env:USERPROFILE\.editorconfig"
+  Remove-Item -Path $env:USERPROFILE\.editorconfig -Force
+}
+
+# Copy new editorconfig file
+Write-Host "Copy .editorconfig to $env:USERPROFILE"
+Copy-Item -Path .\editorconfig\.editorconfig -Destination $env:USERPROFILE
+
+# ------------------------------
 # Neovim
 # ------------------------------
 
 # Remove old neovim files
 if (Test-Path $env:LOCALAPPDATA\nvim) {
-    Write-Host "Remove $env:LOCALAPPDATA\nvim"
-    Remove-Item -Path $env:LOCALAPPDATA\nvim -Recurse -Force
+  Write-Host "Remove $env:LOCALAPPDATA\nvim"
+  Remove-Item -Path $env:LOCALAPPDATA\nvim -Recurse -Force
 }
 
 # Copy new neovim files
