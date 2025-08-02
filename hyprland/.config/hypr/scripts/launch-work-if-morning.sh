d85c35de40ceef9e0ad0f5cccbefdf2dbc5e876a
Aidan Timson <aidan@timmo.dev>
2025-08-02 14:21:48 +0100

    Upd

--- hyprland/.config/hypr/scripts/launch-work-if-morning.sh
+++ hyprland/.config/hypr/scripts/launch-work-if-morning.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Check if it's morning (between 8am and 11am) and a weekday
-if [ $(date +%H) -ge 8 ] && [ $(date +%H) -lt 11 ] && [ $(date +%u) -lt 6 ]; then
+# Check if it's morning (between 7am and 11am) and a weekday
+if [ $(date +%H) -ge 7 ] && [ $(date +%H) -lt 11 ] && [ $(date +%u) -lt 6 ]; then
   # Launch work setup
   slack &
-  google-chrome-stable --enable-features=UseOzonePlatform --ozone-platform=wayland --ozone-platform-hint=auto --profile-directory="Profile 1" &
+  google-chrome-stable --enable-features=UseOzonePlatform --ozone-platform=wayland --ozone-platform-hint=auto --profile-directory="Profile 3" &
 fi
