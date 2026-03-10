a5e7d8170c39ec1852b2350ea6ede0b726a3e870
Aidan Timson <aidan@timmo.dev>
2026-03-10 15:21:49 +0000

    Update 99

--- neovim/.config/nvim/lua/plugins/99.lua
+++ neovim/.config/nvim/lua/plugins/99.lua
@@ -8,21 +8,46 @@ return {
       md_files = {
         "AGENT.md",
       },
+      -- completion = {
+      --   source = "blink",
+      -- },
     })
 
-    -- Fill in function body with AI
-    vim.keymap.set("n", "<leader>9f", function()
-      _99.fill_in_function()
-    end, { desc = "99: Fill in function" })
-
-    -- Visual selection AI completion
+    -- visual must be set in visual mode; uses the current/last visual selection
     vim.keymap.set("v", "<leader>9v", function()
       _99.visual()
-    end, { desc = "99: Visual AI" })
+    end, { desc = "99: Visual" })
 
-    -- Stop all AI requests
-    vim.keymap.set("n", "<leader>9s", function()
+    vim.keymap.set("n", "<leader>9x", function()
       _99.stop_all_requests()
-    end, { desc = "99: Stop requests" })
+    end, { desc = "99: Stop all" })
+
+    vim.keymap.set("n", "<leader>9s", function()
+      _99.search()
+    end, { desc = "99: Search" })
+
+    vim.keymap.set("n", "<leader>9V", function()
+      _99.vibe()
+    end, { desc = "99: Vibe" })
+
+    vim.keymap.set("n", "<leader>9o", function()
+      _99.open()
+    end, { desc = "99: Open" })
+
+    vim.keymap.set("n", "<leader>9l", function()
+      _99.view_logs()
+    end, { desc = "99: Logs" })
+
+    vim.keymap.set("n", "<leader>9c", function()
+      _99.clear_previous_requests()
+    end, { desc = "99: Clear" })
+
+    vim.keymap.set("n", "<leader>9i", function()
+      _99.info()
+    end, { desc = "99: Info" })
+
+    vim.keymap.set("n", "<leader>9t", function()
+      _99.tutorial()
+    end, { desc = "99: Tutorial" })
   end,
 }
