c8a004e31fd960e6adeba2ad0bd2b4fd8f29b26c
Aidan Timson <aidan@timmo.dev>
2026-07-14 14:19:09 +0100

    Render Markdown code blocks in Neovim

---
+++ neovim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,16 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  opts = {
+    code = {
+      sign = false,
+      width = "block",
+      right_pad = 1,
+    },
+    heading = {
+      sign = false,
+      icons = {},
+    },
+  },
+  ft = { "markdown", "norg", "rmd", "org" },
+}
