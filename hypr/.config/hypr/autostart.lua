8b9f72406c81f5ce38f09e8bd6d2fb1f2d11a9f0
Aidan Timson <aidan@timmo.dev>
2026-09-11 20:20:31 +0100

    Hide KDE Connect tray indicator

--- hypr/.config/hypr/autostart.lua
+++ hypr/.config/hypr/autostart.lua
@@ -3,7 +3,7 @@
 
 o.exec_on_start("timmo-run-command system-bridge backend")
 o.exec_on_start([[uwsm app -- chromium --new-window --ozone-platform=wayland --profile-directory="Default" --force-device-scale-factor=0.8]])
-o.exec_on_start("uwsm app -- kdeconnect-indicator")
+o.exec_on_start("uwsm app -- kdeconnectd")
 o.exec_on_start("uwsm-app -s b -- twitch-notifications")
 o.exec_on_start("dot herdr start")
 
